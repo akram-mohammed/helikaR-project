e3b1f6d9d90aeb4ea6bb78d234dcef13a4c90efe
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2015-06-03 02:07:13 +0530

    integrated mod panel. todo - mean, median, etc.

--- js/components.js
+++ js/components.js
@@ -3,20 +3,52 @@ function getOption(axis) {
 	return select.options[select.selectedIndex].value;
 }
 
+function Column(functionName, preCol) {
 
+    var sum = 0;
+    var i;
+
+    this.functionName = functionName;
+    this.preCol = preCol;
+    this.mean = this.preCol.reduce(function (a, b) { return a + b; }, 0) / this.preCol.length;
+
+    for (i = 0; i < this.preCol.length; i++) {
+        sum += Math.pow(this.preCol[i] - this.mean, 2);
+    }
+
+    this.variance = sum / this.preCol.length;
+    this.sd = Math.sqrt(this.variance);
+
+    // apply function by name
+    this.applyFunction = function () {
+        console.log(this.sd);
+        return this[this.functionName]();
+    };
+
+    // Feature scaling
+    this.fscale = function () {
+        var min = Math.min.apply(null, this.preCol);
+        var max = Math.max.apply(null, this.preCol);
+        return this.preCol.map(function (elem) {
+            return (elem - min) / (max - min);
+        });
+    };
+}
 
 var WholeThing = React.createClass(
 {
 	getInitialState: function() {
-		return {file: "", data: [], path: "", plot: false};
+		return {file: "", data: [], path: ""};
 	},
 
 	getDefaultProps: function() {
-		return {table: null};
+		return {table: null, plot: false};
 	},
 
 	componentDidMount: function() {
-		var container = document.getElementById("hot");
+		//var container = document.getElementById("hot");
+		var container = React.findDOMNode(this.refs.hbut);
+
 	    var hot = new Handsontable(container, {
 	        colHeaders: true,
 	        minSpareRows: 1,
@@ -33,7 +65,7 @@ var WholeThing = React.createClass(
 		{
 			var choices = [];
 			var main_out;
-
+			
 			ocpu.seturl("//public.opencpu.org/ocpu/library/utils/R");
 
 	        // upload
@@ -56,9 +88,11 @@ var WholeThing = React.createClass(
                     	}
                 	});
 
-	                /*andsontable.Dom.addEvent(container, 'click', function (event) {
+        			var container = React.findDOMNode(this.refs.hbut);
+	                Handsontable.Dom.addEvent(container, 'click', function (event) {
 	                    if (event.target.className === 'mod_button') {
-	                        var preDiv = document.getElementById("pre-button-div");
+	                    	console.log("Aha!");
+	                        var preDiv = React.findDOMNode(this.refs.panel);
 
 	                        // slide up current div to show a visual change
 	                        $(preDiv).slideUp("fast");
@@ -67,7 +101,7 @@ var WholeThing = React.createClass(
 	                        // TODO: remove global scope
 	                        preColumnNum = Number(event.target.getAttribute("name"));
 	                    }
-               		});*/
+               		}.bind(this));
 
                 	// load data
                 	hot.loadData(out);
@@ -97,7 +131,7 @@ var WholeThing = React.createClass(
 
 	    }  
 
-	    if(this.state.plot !== prevState.plot) {
+	    if(this.props.plot === true) {
 	    	ocpu.seturl("//public.opencpu.org/ocpu/library/utils/R");
 
 	        // plot
@@ -106,24 +140,29 @@ var WholeThing = React.createClass(
 	        }, function (session) {
 
 	        	ocpu.seturl("//public.opencpu.org/ocpu/library/base/R");
+
 	        	var hot = this.props.table;
 	     		console.log(hot.getData());
 	        	session.getObject(function () {
-	            // plot functions
-	            ocpu.seturl("//ramnathv.ocpu.io/rCharts/R");
-	            var requestText = "nPlot(Weight ~ Height, data = data.frame(jsonlite::fromJSON('[{\"Height\": 20, \"Weight\": 40}, {\"Height\": 40, \"Weight\": 60}, {\"Height\": 60, \"Weight\": 10}]')), type = 'scatterChart')\n";
+	            	// plot functions
+		            ocpu.seturl("//ramnathv.ocpu.io/rCharts/R");
 
-	            var plotRequest = ocpu.call("make_chart", {
-	            	text: requestText
-	            }, function(session2) {
-	            	session2.getConsole(function () {
-	            		var url = session2.getLoc() + "files/output.html";
-	            		this.setState({path: url});
+		            console.log(getOption("y"));
+
+		            var requestText = "nPlot(" + getOption("y") + " ~ " + getOption("x") + ", data = data.frame(jsonlite::fromJSON('" + JSON.stringify(hot.getData()) + "')), type = 'scatterChart')\n";
+
+		            var plotRequest = ocpu.call("make_chart", {
+		            	text: requestText
+	            	}, function(session2) {
+	            		session2.getConsole(function () {
+	            			var url = session2.getLoc() + "files/output.html";
+	            			this.setState({path: url});
+	            		}.bind(this));
 	            	}.bind(this));
-	            }.bind(this));
+	        	}.bind(this));
 	        }.bind(this));
-	        }.bind(this));
-	        //this.setState({file: ""});
+	        // this.setState({file: ""});
+	        this.setProps({plot: false});
 	    }
 
 	    else {
@@ -135,23 +174,80 @@ var WholeThing = React.createClass(
 		return (
         	// get radio buttons
         	<div>
-        	<FileUploader onClick={this.handleClick} />
-        	<ChoicePanel choices={this.state.data} axis="x" />
-        	<PlotWindow path={this.state.path} />
-        	<ChoicePanel choices={this.state.data} axis="y" />
-        	<div id="hot"></div>
+	        	<FileUploader onClick={this.handleClick} />
+	        	<ChoicePanel choices={this.state.data} axis="x" />
+	        	<ChoicePanel choices={this.state.data} axis="y" />
+	        	<div id="plot-panel">
+	        		<PlotWindow path={this.state.path} />
+	        	</div>
+	        	<HButton ref="hbut" />
+	        	<ModificationPanel onClick={this.handleClick} ref="panel" />
         	</div>
         	);
 	},
 
-	handleClick: function(child, buttonType) {
+	handleClick: function(child, buttonType, functionName) {
 		if(buttonType === "submit") {
 			var myFile = $("#input-file")[0].files[0];
 			this.setState({file: myFile});
 		}
-		else {
-			this.setState({plot: true});
+
+		else if(buttonType === "modify") {
+			var i;
+			var hot = this.props.table;
+			console.log(hot);
+	     	var preColArr = hot.getDataAtCol(preColumnNum).filter(function (elem) {
+	            return elem !== null;
+	        });
+	     	console.log("ARR!!" + preColArr);
+
+	        preColArr = new Column(functionName, preColArr);
+	        var postColArr = preColArr.applyFunction();
+	        for (i = 0; i < postColArr.length; i++) {
+	            hot.setDataAtCell(i, preColumnNum, postColArr[i]);
+	        }
 		}
+
+		else {
+			this.setProps({plot: true});
+			this.forceUpdate();
+		}
+
+	}
+});
+
+var ModificationPanel = React.createClass({
+	handleClick: function(child, buttonType, functionName) {
+		this.props.onClick(this, buttonType, functionName);
+	},
+
+	render: function() {
+		return (
+			<div id="pre-button-div" style={{display: 'none'}}>
+				<ModificationButton onClick={this.handleClick} id="fscale" />
+	    	</div>
+		);
+	}
+});
+
+var ModificationButton = React.createClass(
+{
+	handleClick: function(buttonType, functionName) {
+		this.props.onClick(this, buttonType, functionName);
+	},
+
+	render: function() {
+		return (
+			<button id="{this.props.id}" onClick={this.handleClick.bind(this, "modify", this.props.id)}>this.props.name</button>
+		);
+	}
+});
+
+var HButton = React.createClass({
+	render: function() {
+		return (
+			<div></div>
+		);
 	}
 });
 
@@ -187,7 +283,7 @@ var PlotWindow = React.createClass(
 					var plotArr = plotHTML.split("<head>");
 
 					//temp static stuff
-					var squeezeFrame = '<head>\n<script type="text/javascript" src="js/squeezeFrame.js"></script>\n<script type="text/javascript">\n\tmyContainer="localhost/Statistical%20Computing/components.html";\n\tmyMax=0.25;\n\tmyRedraw="both";\n</script>';
+					var squeezeFrame = '<head>\n<script type="text/javascript" src="js/squeezeFrame.js"></script>\n<script type="text/javascript">\n\tmyContainer="localhost/Statistical Computing/components.html";\n\tmyMax=0.25;\n\tmyRedraw="both";\n</script>';
 
 					var plotFrame = document.getElementById("plot-frame").contentWindow.document;
 
@@ -219,7 +315,7 @@ var ChoicePanel = React.createClass(
 					</option>);
 		}.bind(this));
 		return (
-			<select id="variable-select-{this.props.axis}">{clist}</select>
+			<select id={"variable-select-" + this.props.axis}>{clist}</select>
 		);
 	}
 });
