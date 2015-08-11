a1aede4bda37bd633db00e5462c39dd907ccb061
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2015-08-11 18:06:12 +0530

    small updates

--- inst/js/classifiers.js
+++ inst/js/classifiers.js
@@ -12,11 +12,56 @@ function naiveBayesClassify(bundle) {
 		return d[classify_var] === "";
 	});
 
-	console.log(JSON.stringify(train));
-	
+	var train_y = train.map(function (d) {
+		return d[classify_var];
+	})
+
+	var test_y = test.map(function (d) {
+		return d[classify_var];
+	})
+
+	var train_x = train;
+	var test_x = test;
+
+	train_x.forEach(function (f) {
+		delete f[classify_var];
+	});
+
+	test_x.forEach(function (f) {
+		delete f[classify_var];
+	});
+
+	//var test_x = test;
+
+
+	console.log(JSON.stringify(train_x));
+	console.log(JSON.stringify(train_y));
+
 	var x, y;
 
-	ocpu.seturl("//public.opencpu.org/ocpu/library/base/R");
+	ocpu.seturl("//public.opencpu.org/ocpu/library/e1071/R");
+	ocpu.call("naiveBayes", {
+		// TODO: filter out null
+		x: train_x,
+		y: train_y
+	}, function (session1) {
+
+		// START
+
+		ocpu.seturl("//public.opencpu.org/ocpu/library/stats/R");
+		ocpu.call("predict", {
+			object: session1,
+			newdata: test_x
+		}, function (session2) {
+			session2.getObject(null, {force: true}, function (obj) {
+				console.log(obj);
+			});
+		});
+
+		// END
+	});
+
+	/*ocpu.seturl("//public.opencpu.org/ocpu/library/base/R");
 	var xcall = ocpu.rpc("subset", {
 		x: new ocpu.Snippet("na.omit(jsonlite::fromJSON('" + JSON.stringify(train) + "'))"),
 		select: new ocpu.Snippet("-" + classify_var)
@@ -34,8 +79,8 @@ function naiveBayesClassify(bundle) {
 
 				ocpu.seturl("//public.opencpu.org/ocpu/library/e1071/R");
 				ocpu.call("naiveBayes", {
-					x: x,
-					y: y
+					x: x[0],
+					y: y[0]
 				}, function (session1) {
 					
 					// START
@@ -58,6 +103,6 @@ function naiveBayesClassify(bundle) {
 		});
 		// END
 
-	});
+	});*/
 
 }
