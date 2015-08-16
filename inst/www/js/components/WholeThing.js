6f545dc2530d750a6bc956c1148d3d15da21c4eb
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2015-08-16 20:50:07 +0530

    namespace

--- inst/www/js/components/WholeThing.js
+++ inst/www/js/components/WholeThing.js
@@ -16,7 +16,7 @@ var WholeThing = React.createClass(
 
 		console.log("Custom call: ");
 
-		ocpu.call("Helikar::myfn", {
+		ocpu.call("myfn", {
 			x: 512
 		}, function (session) {
 			session.getObject(function (obj) {
