71c0d49558c3a1c9314e94586339392b7c2db643
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2015-08-17 12:24:05 +0530

    added factor conversion

--- R/myfn.R
+++ R/myfn.R
@@ -3,10 +3,11 @@
 #' @export
 #' @import e1071
 #' @import stats
+#' @import base
 #' @param tr_x train x
 #' @param tr_y train y
 #' @param te_x test x
 #' @param te_y test y
 myfn <- function(tr_x, tr_y, te_x, te_y) {
-	return(table(predict(naiveBayes(tr_x, tr_y), te_x), te_y));
+	return(table(predict(naiveBayes(tr_x, tr_y), as.factor(te_x)), te_y));
 }
