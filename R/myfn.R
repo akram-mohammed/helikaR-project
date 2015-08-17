b1a1eccc4b8e546318ae9f1519ffd49dfa5893ef
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2015-08-17 12:27:19 +0530

    moved as.factor

--- R/myfn.R
+++ R/myfn.R
@@ -8,5 +8,5 @@
 #' @param te_x test x
 #' @param te_y test y
 myfn <- function(tr_x, tr_y, te_x, te_y) {
-	return(table(predict(naiveBayes(tr_x, tr_y), as.factor(te_x)), te_y));
+	return(table(predict(naiveBayes(tr_x, as.factor(tr_y)), te_x), te_y));
 }
